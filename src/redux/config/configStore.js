ca132c0383b854845d11d15c498fc4c9b2a18dfa
baram55 <asd7973@gmail.com>
2024-02-20 12:12:20 +0900

    [feat]tookit redux로 변경

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,10 +1,8 @@
-import { createStore, combineReducers } from "redux";
-import letters from "redux/modules/letters";
-import member from "redux/modules/member";
-import { devToolsEnhancer } from "redux-devtools-extension";
-
-const rootReducer = combineReducers({ letters, member });
-
-const store = createStore(rootReducer, devToolsEnhancer());
+import { configureStore } from "@reduxjs/toolkit";
+import letters from "../modules/letters";
+import member from "../modules/member";
+const store = configureStore({
+  reducer: { letters, member },
+});
 
 export default store;
