7edbb36fe627db3fb61da02e1fd416accc1c74eb
baram55 <asd7973@gmail.com>
2024-02-20 12:57:30 +0900

    [feat]회원가입 페이지 삭제

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -1,27 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
 export const LogIn = () => {
+  const [isLogInMode, setIsLogInMode] = useState(true);
   const navigate = useNavigate();
-  return (
+  return isLogInMode ? (
     <BackGround>
       <LogInFrame>
         <Title>로그인</Title>
         <InputId
           type="text"
+          minLength="4"
           maxLength="10"
           placeholder="아이디 (4~10글자)"
-        ></InputId>
+        />
         <InputPw
           type="password"
+          minLength="4"
           maxLength="15"
           placeholder="비밀번호 (4~15글자)"
-        ></InputPw>
+        />
         <LogInButton>로그인</LogInButton>
-        <RegisterLink onClick={() => navigate("/register")}>
+        <RegisterLink onClick={() => setIsLogInMode(false)}>
           회원가입
         </RegisterLink>
+      </LogInFrame>
+    </BackGround>
+  ) : (
+    <BackGround>
+      <LogInFrame>
+        <Title>회원가입</Title>
+        <InputId
+          type="text"
+          minLength="4"
+          maxLength="10"
+          placeholder="아이디 (4~10글자)"
+        />
+        <InputPw
+          type="password"
+          minLength="4"
+          maxLength="15"
+          placeholder="비밀번호 (4~15글자)"
+        />
+        <InputNickname
+          type="text"
+          minLength="1"
+          maxLength="10"
+          placeholder="닉네임 (1~10글자)"
+        />
+        <LogInButton>회원가입</LogInButton>
+        <RegisterLink onClick={() => setIsLogInMode(true)}>로그인</RegisterLink>
       </LogInFrame>
     </BackGround>
   );
@@ -61,6 +90,11 @@ const InputPw = styled.input`
   margin-bottom: 20px;
 `;
 
+const InputNickname = styled.input`
+  height: 50px;
+  margin-bottom: 20px;
+`;
+
 const LogInButton = styled.button`
   background-color: gray;
   color: white;
@@ -70,6 +104,24 @@ const LogInButton = styled.button`
 `;
 
 const RegisterLink = styled.p`
+  color: gray;
+  text-align: center;
+  margin-top: 20px;
+  cursor: pointer;
+  &:hover {
+    color: black;
+  }
+`;
+
+const RegisterButton = styled.button`
+  background-color: gray;
+  color: white;
+  height: 100px;
+  font-size: 30px;
+  cursor: pointer;
+`;
+
+const LogInLink = styled.p`
   color: gray;
   text-align: center;
   margin-top: 20px;
--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,7 +1,6 @@
 import Detail from "pages/Detail";
 import Home from "pages/Home";
 import { LogIn } from "pages/LogIn";
-import { Register } from "pages/Register";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 export default function Router() {
@@ -10,8 +9,7 @@ export default function Router() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/detail/:id" element={<Detail />} />
-        <Route path="/log_in" element={<LogIn />} />
-        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<LogIn />} />
         <Route path="*" element={<Navigate replace to="/" />} />
       </Routes>
     </BrowserRouter>
